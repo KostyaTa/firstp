77803bfba0a7007b8a023ccba7d7b21223fee9cb
KostyaTa <kostyata@gmail.com>
2018-04-22 17:00:19 +0300

    sending mail

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -3,19 +3,44 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class FormController extends Controller
+class FormController extends AbstractController
 {
     /**
      * @Route("/form", name="form")
      */
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
         $form = $this->createForm(ContactType::class);
 
         dump($form);
+
+        $form->handleRequest($request);
+
+        $this->addFlash('succses', 'message send');
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $data = $form->getData();
+
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom($data['email'])
+                ->setTo('kostyata@gmail.com')
+                ->setBody(
+                        $data['message'],
+                    'text/plain'
+                    );
+
+            $mailer->send($message);
+            $mailer->send($message);
+
+            return $this->redirectToRoute("form");
+
+            dump($data);
+        }
 
         return $this->render('form/index.html.twig', [
             'controller_name' => 'FormController',
@@ -23,3 +48,4 @@ class FormController extends Controller
         ]);
     }
 }
+
